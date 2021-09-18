d178beb75e870a44fcbe194a4327f344bbef4b13
anwar <anwarhussein2353@gmail.com>
2021-09-18 09:55:15 -0500

    axios get method working

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,5 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }, {id: 2, path: '', description: 'swimming pool prank.', likes: 3}
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,48 @@
+import axios from 'axios';
 import React from 'react';
+import { useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [galleryList, setGalleryList] = useState([]);
+
+  
+  const fetchImages =() =>{
+
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    }).then((response) =>{
+        console.log(response.data);
+        setGalleryList(response.data);
+    }).catch((error) =>{
+      alert('Error making GET request.' , error);
+    });
+  }
+
+  useEffect(() =>{
+    fetchImages();
+  }, []);
+
     return (
+      <>
+      
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        {/* <img src="images/goat_small.jpg"/> */}
+        <ul>
+        {galleryList.map(item =>
+                 <img src={item.path}/>
+          )}
+
+        </ul>
       </div>
+      
+      </>
     );
 }
 
