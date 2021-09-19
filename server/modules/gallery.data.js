24df884cee92ca721f130a373e11e66f11a3cc5d
anwar <anwarhussein2353@gmail.com>
2021-09-19 11:10:35 -0500

    Added a photo to the gallery

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,5 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }, {id: 2, path: 'images/halfcamelhead.jpg', description: 'Half Camel.', likes: 3}, {id: 3, path: 'images/mzeeMzima.jpg', description: 'An oldman listening to BBC Somali in the desert.', likes: 5 },{id: 4, path: 'images/wells.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },{ id: 5, path: 'images/anwarH.jpg', description: 'Photo showing my sitting room.', likes:2 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }, {id: 2, path: 'images/halfcamelhead.jpg', description: 'Half Camel.', likes: 3}, {id: 3, path: 'images/mzeeMzima.jpg', description: 'An oldman listening to BBC Somali in the desert.', likes: 5 },{id: 4, path: 'images/wells.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },{ id: 5, path: 'images/anwarH.jpg', description: 'Photo showing my sitting room.', likes:2 },{id: 6, path: 'images/computer.jpg', description: 'Photo of my workstation.', likes: 0 }
 
 ];
 
